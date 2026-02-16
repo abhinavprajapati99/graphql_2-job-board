e28630687c17996190963ee7c5a4fd45bd2d39bc
Abhinav Prajapati <abhinavprajapati50@gmail.com>
2026-02-16 12:59:44 +0530

    fetch data from the squilite db table and add another field and render it

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,11 +1,15 @@
+import { getJobs } from "./db/jobs.js";
 export const resolvers = {
     Query: {
-        job: () => {
-            return {
-                id: "test-Id",
-                title: "THe JOB",
-                description: "The DESC"
-            }
-        }
+        jobs: async () => getJobs()
+
+    },
+
+    Job: {
+        date: (job) => toIsoDate(job.createdAt)
     }
+}
+
+function toIsoDate(value) {
+    return value.slice(0, 'yyy-mm-dd'.length)
 }
